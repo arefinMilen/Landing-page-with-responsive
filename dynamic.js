8b2d3c5def9c5fef8a8110899efaefb01ef044b2
arefinMilen <arefin15-5279@diu.edu.bd>
2024-10-19 18:05:21 +0600

    completed full page responsiveness

---
+++ dynamic.js
@@ -0,0 +1,11 @@
+// function changeImage(x,image)
+// {
+//     if(x == 1)
+//     {
+//         image.src = 'coconut-water-collection-image_v2_690x.webp';
+//     }
+//     if(x == 2)
+//     {
+//         image.src = '/photo/coconut-water-hover.jpg';
+//     }
+// }
